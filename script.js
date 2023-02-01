dabe1fed7c39dcb3ef82eafa4b415854e8e6d504
Guilherme Medon <113711968+Gmedon@users.noreply.github.com>
2023-02-01 18:07:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,99 +1,114 @@
-var nav = document.querySelector('nav');
-var button = document.querySelector('.hamburger-menu')
-button.addEventListener('click', (event) => {
-    nav.classList.toggle('open')
-})
+var nav = document.querySelector("nav");
+var button = document.querySelector(".hamburger-menu");
+button.addEventListener("click", (event) => {
+  nav.classList.toggle("open");
+});
 
-document.querySelector('.botao__cor__perseguida').onmousemove = (e) => {
+document.querySelector(".botao__cor__perseguida").onmousemove = (e) => {
+  const x = e.pageX - e.target.offsetLeft;
+  const y = e.pageY - e.target.offsetTop;
 
-    const x = e.pageX - e.target.offsetLeft
-    const y = e.pageY - e.target.offsetTop
-
-    e.target.style.setProperty('--x', `${x}px`)
-    e.target.style.setProperty('--y', `${y}px`)
-
-}
+  e.target.style.setProperty("--x", `${x}px`);
+  e.target.style.setProperty("--y", `${y}px`);
+};
 
 function reveal() {
-    var reveals = document.querySelectorAll('.reveal');
+  var reveals = document.querySelectorAll(".reveal");
 
-    reveals.forEach((reveal) => {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveal.getBoundingClientRect().top;
-        var elementVisible = 100;
+  reveals.forEach((reveal) => {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal.getBoundingClientRect().top;
+    var elementVisible = 100;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveal.classList.add('active');
-        } else {
-            reveal.classList.remove("active");
-        }
-    });
+    if (elementTop < windowHeight - elementVisible) {
+      reveal.classList.add("active");
+    } else {
+      reveal.classList.remove("active");
+    }
+  });
 }
-window.addEventListener("scroll", reveal)
+window.addEventListener("scroll", reveal);
 
 function reveal2() {
-    var reveals = document.querySelectorAll('.reveal2');
+  var reveals = document.querySelectorAll(".reveal2");
 
-    reveals.forEach((reveal2) => {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveal2.getBoundingClientRect().top;
-        var elementVisible = 100;
+  reveals.forEach((reveal2) => {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal2.getBoundingClientRect().top;
+    var elementVisible = 100;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveal.classList.add('active');
-        } else {
-            reveal.classList.remove("active");
-        }
-    });
+    if (elementTop < windowHeight - elementVisible) {
+      reveal.classList.add("active");
+    } else {
+      reveal.classList.remove("active");
+    }
+  });
 }
-window.addEventListener("scroll", reveal2)
+window.addEventListener("scroll", reveal2);
 
 let docTitle = document.title;
 window.addEventListener("blur", () => {
-    document.title = "Volte aqui :( "
-})
+  document.title = "Volte aqui :( ";
+});
 window.addEventListener("focus", () => {
-    document.title = docTitle
-})
+  document.title = docTitle;
+});
 
-let urlcalculador = "https://calculadora-pink.vercel.app/"
-let urlemunah = "https://formulario-emunah.vercel.app/"
-let urlsemaforo = "https://projeto-semaforo.vercel.app/"
-let urlcifra = "https://codificar-cifracesar-base-64.vercel.app/"
+let urlcalculador = "https://calculadora-pink.vercel.app/";
+let urlemunah = "https://formulario-emunah.vercel.app/";
+let urlsemaforo = "https://projeto-semaforo.vercel.app/";
+let urlcifra = "https://codificar-cifracesar-base-64.vercel.app/";
 function entrarcalculadora() {
-    let win = window.open(urlcalculador, '_blank')
-    win.focus()
+  let win = window.open(urlcalculador, "_blank");
+  win.focus();
 }
 function entraremunah() {
-    let win = window.open(urlemunah, '_blank')
-    win.focus()
+  let win = window.open(urlemunah, "_blank");
+  win.focus();
 }
 function entrasemaforo() {
-    let win = window.open(urlsemaforo, '_blank')
-    win.focus()
+  let win = window.open(urlsemaforo, "_blank");
+  win.focus();
 }
 function entrarcifra() {
-    let win = window.open(urlcifra, '_blank')
-    win.focus()
+  let win = window.open(urlcifra, "_blank");
+  win.focus();
 }
 function sobre() {
-    document.querySelector(".sobre-enviar").scrollIntoView({ behavior: 'smooth' })
+  document
+    .querySelector(".sobre-enviar")
+    .scrollIntoView({ behavior: "smooth" });
 }
 
 function skills() {
-    document.querySelector(".container-skills").scrollIntoView(-100, { behavior: 'smooth' })
+  document
+    .querySelector(".container-skills")
+    .scrollIntoView(-100, { behavior: "smooth" });
 }
 
 function projetos() {
-    document.querySelector(".projetos").scrollIntoView(-100, { behavior: 'smooth' })
+  document
+    .querySelector(".projetos")
+    .scrollIntoView(-100, { behavior: "smooth" });
 }
 
 function contatos() {
-    document.querySelector("footer").scrollIntoView(-100, { behavior: 'smooth' })
+  document.querySelector("footer").scrollIntoView(-100, { behavior: "smooth" });
 }
 
-let urlcv = "https://drive.google.com/file/d/14_4_FIXbxw1orCsqKujPJS6-U3jzJEty/view?usp=share_link"
+let urlcv =
+  "https://drive.google.com/file/d/14_4_FIXbxw1orCsqKujPJS6-U3jzJEty/view?usp=share_link";
 function dowloadcv() {
-    let win = window.open(urlcv, '_blank')
-    win.focus()
+  let win = window.open(urlcv, "_blank");
+  win.focus();
 }
+
+let foto = document.querySelector(".post-foto");
+
+foto.addEventListener("mouseover", () => {
+  foto.setAttribute("src", "img/texto-sobre.png");
+});
+
+foto.addEventListener("mouseout", () => {
+  foto.setAttribute("src", "img/fotominha.png");
+});
